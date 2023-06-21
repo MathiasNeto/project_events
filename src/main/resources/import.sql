2e7ba148d973d6f5745ec67851322500c0e2a6ff
Emanuel Matias <matias.neto.edu@gmail.com>
2023-06-20 23:10:09 -0300

    created seeding: category and activities

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
 INSERT INTO tb_category(description) VALUES ('Curso');
 INSERT INTO tb_category(description) VALUES ('Oficina');
+
+INSERT INTO tb_activity(name, description, price, category_id) VALUES ('Curso de HTML', 'Aprenda HTML de forma pratica', 80.00, 1);
+INSERT INTO tb_activity(name, description, price, category_id) VALUES ('Oficina de GitHub', 'Controle versoes de seus projetos', 50.00, 2);
