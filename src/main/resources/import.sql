837fb9090b76326b36e77c99209950cfb5684bc6
Emanuel Matias <matias.neto.edu@gmail.com>
2023-06-25 13:22:06 -0300

    Update seeding DB

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,3 +12,11 @@ INSERT INTO tb_participant(name, email) VALUES ('Jose Silva', 'jose@gmail.com');
 INSERT INTO tb_participant(name, email) VALUES ('Tiago Faria', 'tiago@gmail.com');
 INSERT INTO tb_participant(name, email) VALUES ('Maria do Rosario', 'maria@gmail.com');
 INSERT INTO tb_participant(name, email) VALUES ('Teresa Silva', 'teresa@gmail.com');
+
+
+INSERT INTO tb_activity_participant(participant_id, activity_id) VALUES (1,1);
+INSERT INTO tb_activity_participant(participant_id, activity_id) VALUES (1,2);
+INSERT INTO tb_activity_participant(participant_id, activity_id) VALUES (2,1);
+INSERT INTO tb_activity_participant(participant_id, activity_id) VALUES (3,1);
+INSERT INTO tb_activity_participant(participant_id, activity_id) VALUES (3,2);
+INSERT INTO tb_activity_participant(participant_id, activity_id) VALUES (4,2);
